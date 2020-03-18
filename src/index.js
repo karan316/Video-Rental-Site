eb9a8378615d93527fe0261c9be2d9bb234ecadc
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-03-18 21:34:44 +0530

    added env variables

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import { BrowserRouter } from "react-router-dom";
 
+console.log("SUPERMAN", process.env.REACT_APP_NAME);
+
 logger.init();
 
 ReactDOM.render(
